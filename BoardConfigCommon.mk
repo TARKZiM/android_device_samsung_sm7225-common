f43b5ee0322420f3ddb3a080694ce6f3a8898210
TARKZiM <tom8476oo@gmail.com>
2022-07-15 00:41:59 +0800

    sm7225-common: sepolicy: Update from SM7125 tree
    * https://github.com/Simon1511/android_device_samsung_sm7125-common

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -201,6 +201,7 @@ TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.default
 # SePolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 
 # Treble
 BOARD_VNDK_VERSION := current
