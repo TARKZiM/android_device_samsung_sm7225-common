f727e2475428b1d0751fdd41a34d66e9d093fee9
roynatech2544 <whiteshell2544@naver.com>
2023-11-14 02:56:28 +0000

    sm7225-common: Build telephony-ext matching stock

--- common.mk
+++ common.mk
@@ -400,6 +400,12 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
+PRODUCT_PACKAGES += \
     PhhIms \
     PhhImsOverlay
 
