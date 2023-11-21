e091ebe7e64a89fbf51a807fa2fd18bd1aec03db
TARKZiM <tom8476oo@gmail.com>
2023-11-21 18:13:47 +0800

    sm7225-common: Build our own Camera HAL

--- common.mk
+++ common.mk
@@ -113,13 +113,6 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.6-service.sm7225 \
-    android.hardware.camera.provider@2.4-legacy \
-    android.hardware.camera.provider@2.5-legacy \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    camera.device@3.3-impl \
-    camera.device@3.4-impl \
-    camera.device@3.5-impl \
     libgrallocusage.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     android.hardware.camera.device@3.6.vendor \
