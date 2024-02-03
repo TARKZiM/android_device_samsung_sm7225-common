26ddbf54df620396d8663a158623a2525817f1d3
Simon1511 <me@simon1511.de>
2024-02-03 14:09:56 +0100

    sm7225-common: enable needs_sec_reserved_field for camera
    Change-Id: Ib19af89da1aa47972e4fd2cea0049e362749ae0e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -151,6 +151,9 @@ BOARD_SUPPORTS_SOUND_TRIGGER := true
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/bluetooth/include
 
+# Camera
+$(call soong_config_set,samsungCameraVars,needs_sec_reserved_field,true)
+
 # FOD
 TARGET_USES_FOD_ZPOS := true
 TARGET_SEC_FP_REQUEST_TOUCH_EVENT := true
