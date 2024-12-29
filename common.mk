95e081079cde80a4424c4ab81bf63e5c986c7e07
Alexander Winkowski <dereference23@outlook.com>
2024-12-30 00:12:04 +0100

    sm7225-common: Import cleaned up powerhint
    Signed-off-by: Arjun-Ingole <arjuningole38@gmail.com>
    Change-Id: I3d16e41487873dd25e51f0c8c9e2ecc0642b3c42

--- common.mk
+++ common.mk
@@ -362,6 +362,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr \
     android.hardware.power@1.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
