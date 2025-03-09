ebbd35c34b05ef4cf1fce409682361543b1c8a76
Bruno Martins <bgcngm@gmail.com>
2025-03-09 16:14:43 +0000

    sm7225-common: Drop TARGET_KEYMASTER_VARIANT flag and set soong params
    Samsung keymaster HAL moved to blueprint.
    
    Change-Id: I992a12f1ddc5769ddb9fb232ea2a287c1d654da6

--- common.mk
+++ common.mk
@@ -238,6 +238,8 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0-service.samsung \
     libkeymaster4_1support.vendor
 
+$(call soong_config_set,samsungVars,target_keymaster4_library,//vendor/samsung/sm7225-common:libskeymaster4device)
+
 # Light
 PRODUCT_PACKAGES += \
     android.hardware.light-V1-ndk_platform.vendor:64
