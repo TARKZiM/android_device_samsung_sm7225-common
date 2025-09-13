48c5639a1a27a3c392d56518f3d8ec32e8d8fe1c
TARKZiM <tom8476oo@gmail.com>
2025-09-14 02:41:29 +0800

    sm7225-common: Switch to hardware/samsung camera provider

--- common.mk
+++ common.mk
@@ -113,7 +113,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service.samsung \
+    android.hardware.camera.provider-service.samsung \
     libgrallocusage.vendor \
     libui_shim \
     vendor.qti.hardware.camera.device@1.0 \
