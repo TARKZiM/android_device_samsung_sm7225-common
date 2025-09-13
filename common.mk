f020c93e2fde042d07850adb0c7d3c13307e5dff
TARKZiM <tom8476oo@gmail.com>
2025-09-13 19:25:58 +0800

    sm7225-common: Switch source built display composer

--- common.mk
+++ common.mk
@@ -150,6 +150,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     gralloc.qcom \
     vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.composer-service \
     android.hardware.renderscript@1.0-impl \
     android.hardware.graphics.composer@2.1.vendor \
     android.hardware.graphics.composer@2.2.vendor \
--- extract-files.sh
+++ extract-files.sh
@@ -69,13 +69,13 @@ function blob_fixup() {
             # Replace SlotID prop
             sed -i 's/ril.dds.call.ongoing/vendor.calls.ongoing/g' "${2}"
             ;;
-        vendor/lib64/hw/gatekeeper.mdfpp.so|vendor/lib64/libskeymaster4device.so|vendor/lib64/libkeymaster_helper.so|vendor/lib/libwvhidl.so)
+        vendor/lib64/hw/gatekeeper.mdfpp.so|vendor/lib64/libskeymaster4device.so|vendor/lib64/libkeymaster_helper.so|vendor/lib/libwvhidl.so|vendor/lib/mediadrm/libwvdrmengine.so)
             "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
             ;;
         vendor/lib64/unihal_main@2.1.so|vendor/lib64/libscaler_hw.unifunc.so|vendor/lib/libscaler_hw.unifunc.so)
             "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
             ;;
-        vendor/lib64/vendor.samsung.hardware.light-V1-ndk_platform.so)
+        vendor/lib64/vendor.samsung.hardware.light-V1-ndk_platform.so|vendor/bin/hw/vendor.samsung.hardware.light-service)
             "${PATCHELF}" --replace-needed "android.hardware.light-V1-ndk_platform.so" "android.hardware.light-V1-ndk.so" "${2}"
             ;;
         vendor/lib/libsynaFpSensorTestNwd.so|vendor/lib64/libsynaFpSensorTestNwd.so)
